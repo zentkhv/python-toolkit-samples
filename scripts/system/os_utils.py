c37279ce52b52cd49938f2b47e4f97164b0be200
zentkhv <m.ilchenko2011@mail.ru>
2025-02-06 10:32:27 +1000

    Shell comand execute

--- scripts/system/os_utils.py
+++ scripts/system/os_utils.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
 import os
+import subprocess
 
 
 def get_environment_variables():
     return os.environ
 
+
 def execute_command(command):
     return os.system(command)
+
+
+def execute_shell_command(command: str):
+    result = subprocess.run(command, shell=True, capture_output=True)
+    result = result.stdout.decode('utf-8')  # result.returncode
+    return result
 
 
 if __name__ == '__main__':
